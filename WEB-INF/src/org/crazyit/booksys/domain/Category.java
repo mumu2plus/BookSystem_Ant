049202d3f22f96b15a6089ceb9e7afe7572db8f7
mumu2plus <mumu2plus@gmail.com>
2018-10-08 11:00:23 +0800

    first commit

---
+++ WEB-INF/src/org/crazyit/booksys/domain/Category.java
@@ -0,0 +1,14 @@
+package org.crazyit.booksys.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category_tb")
+public class Category {
+
+}
