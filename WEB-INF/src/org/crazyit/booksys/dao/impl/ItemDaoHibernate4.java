ec0725d53261660d5421193bb00ff927c841276b
mumu2plus <mumu2plus@gmail.com>
2018-10-11 01:04:15 +0800

    normal commit

--- WEB-INF/src/org/crazyit/booksys/dao/impl/ItemDaoHibernate4.java
+++ WEB-INF/src/org/crazyit/booksys/dao/impl/ItemDaoHibernate4.java
@@ -6,5 +6,11 @@ import org.crazyit.booksys.dao.ItemDao;
 import org.crazyit.booksys.domain.Item;
 import org.crazyit.common.dao.impl.BaseDaoHibernate4;
 
-public class ItemDaoHibernate4 {
+public class ItemDaoHibernate4 extends BaseDaoHibernate4<Item>
+    implements ItemDao{
+    @Override
+    public List<Item> findByInventoryId(Integer inventoryId) {
+        return find("select it from Item as it where it.inventory.id = ?0"
+                , inventoryId);
+    }
 }
--- WEB-INF/src/org/crazyit/booksys/domain/Category.java
+++ WEB-INF/src/org/crazyit/booksys/domain/Category.java
@@ -10,5 +10,36 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "category_tb")
 public class Category {
+    @Id @Column(name = "cate_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    private String name;
+
+    @Column(name = "cate_desc")
+    private String desc;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
